ba354a88aac66c90b4a57f03f13f4b388a273c77
Unal Canpolat <34675955+unalcan123@users.noreply.github.com>
2021-11-13 19:08:53 +0100

    ok

--- lib/dersler/fonksiyonlar.dart
+++ lib/dersler/fonksiyonlar.dart
@@ -2,6 +2,14 @@ void main() {
   sayilariTopla();
   int fark = sayilariCikar(15, 4);
   print('fark:$fark');
+  int carpim = sayilariCarp(10, 20);
+  print('carpim: $carpim');
+  print('buyuk olan sayi:' + maxolanibul(5, 20).toString());
+  int ikinciYolBuyukBayiIcin = maxolanibulikinciYol(20, 100);
+  print('ikinci yoldan buyuk sayi bulma:$ikinciYolBuyukBayiIcin');
+  //
+  int minsayi = minolansayiyiBulma(1000, 200);
+  print('min sayi bulma:$minsayi');
 }
 
 void sayilariTopla() {
@@ -12,3 +20,16 @@ void sayilariTopla() {
 int sayilariCikar(int s1, int s2) {
   return s1 - s2;
 }
+
+int sayilariCarp(int s1, int s2) => s1 * s2;
+
+int maxolanibul(int s1, int s2) {
+  if (s1 > s2) {
+    return s1;
+  } else {
+    return s2;
+  }
+}
+
+int maxolanibulikinciYol(int s1, int s2) => (s1 > s2) ? s1 : s2;
+int minolansayiyiBulma(int s1, int s2) => (s1 < s2) ? s1 : s2;
